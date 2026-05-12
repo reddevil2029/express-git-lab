2b6b3a21936dbebe060b48e4534ca1c4cc58c623
Korn <kornthebkk@gmail.com>
2026-05-12 20:18:05 +0700

    feat: update main message by A

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World! This is the main branch.');
+    res.send('Hello World from Account A!');
 });
 
 app.get('/about', (req, res) => {
